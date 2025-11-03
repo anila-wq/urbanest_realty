6ee4d2a92199a7e1a3e312a25105d681afd0d265
Anila <arena.anila@gmail.com>
2025-11-03 16:59:10 +0530

    Updates

--- assets/star-BXmKwUOr.js
+++ assets/star-BXmKwUOr.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Ct0ckvGc.js";/**
+import{c as a}from"./index-Dn1P6lJ1.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
