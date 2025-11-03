8e8cdbc62f3cfb3d185bc00980eb8835aec4da74
Anila <arena.anila@gmail.com>
2025-11-03 17:35:51 +0530

    Updates

--- assets/star-C7gMxNNh.js
+++ assets/star-C7gMxNNh.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Dn1P6lJ1.js";/**
+import{c as a}from"./index-CXtrMmeS.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
