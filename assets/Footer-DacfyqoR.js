7ab956408e63c05331543bbd0028703be9b9145e
Anila <arena.anila@gmail.com>
2025-11-10 10:40:02 +0530

    Updates

--- assets/Footer-DacfyqoR.js
+++ assets/Footer-DacfyqoR.js
@@ -1,4 +1,4 @@
-import{c as a,r as l,j as e,P as c,M as o,g as h,E as d}from"./index-BFqfDyeL.js";/**
+import{c as a,r as l,j as e,P as c,M as o,g as h,E as d}from"./index-BgfxqKPB.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/star-CmRY-QS7.js
+++ assets/star-CmRY-QS7.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BFqfDyeL.js";/**
+import{c as a}from"./index-BgfxqKPB.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
